9acb3ebf400ea9caa0d54cc7e9ca8b0f8f644437
bugong <1761869346@qq.com>
2018-06-05 23:12:23 +0800

    调整项目结构

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,12 @@ extern crate actix_web;
 use actix_web::{server, App, HttpRequest, HttpResponse};
 
 fn main() {
-
-    server::new(|| {
-        App::new()
-            .resource("/", |r| {r.f(index)})
-    })
-    .bind("127.0.0.1:8888")
-    .expect("can't bing to port 8888")
-    .run();
+    server::new(|| App::new().resource("/", |r| r.f(index)))
+        .bind("127.0.0.1:8888")
+        .expect("can't bing to port 8888")
+        .run();
 }
 
 fn index(req: HttpRequest) -> &'static str {
-
     "Hello world"
 }
