72aaa6158a0e7f0e07ce97bd38182c6b0a59c89f
bugong <1761869346@qq.com>
2018-07-24 23:53:29 +0800

    fixing filter

--- src/common/filter.rs
+++ src/common/filter.rs
@@ -1,21 +1,73 @@
-use actix_web::Request;
+use actix::Arbiter;
+use actix_web::{Error, Request};
 use actix_web::pred::Predicate;
 use actix_web::middleware::session::RequestSession;
+use actix_redis::{Command, RespValue};
+use cookie::{Cookie, CookieJar, Key};
+use futures::Future;
 
+use common::lazy_static::CONFIG;
+use common::state::AppState;
 use models::user::RawUser;
 
 pub struct Unauthorized;
 
 pub struct CheckLogin;
 
-impl<S> Predicate<S> for CheckLogin {
-    
-    fn check(&self, req: &Request, state: &S) -> bool {
+impl Predicate<AppState> for CheckLogin {
 
-//        req.extensions_mut().insert(Unauthorized);
-//
-//        req.session().get::<RawUser>("user").unwrap().is_some()
+    fn check(&self, req: &Request, state: &AppState) -> bool {
 
-        true
+        req.extensions_mut().insert(Unauthorized);
+
+        false
     }
+}
+
+fn do_check(req: &Request, state: &AppState) -> bool {
+
+    let cookie = req.headers().get("cookie");
+
+    if cookie.is_none() {
+
+        return false;
+    }
+
+    let cookie_str = cookie.unwrap().to_str().unwrap();
+
+    let cookies = Cookie::parse(cookie_str);
+
+    for cookie in cookies {
+
+        if cookie.name() == &*CONFIG.cookie.key {
+
+            let mut jar = CookieJar::new();
+            jar.add_original(cookie.clone());
+
+            if let Some(cookie) = jar.signed(&Key::from_master(&[0;32])).get(&*CONFIG.cookie.key) {
+
+                let addr = state.redis_addr.clone();
+                let result = Arbiter::spawn_fn(move || {
+                    addr
+                        .send(Command(resp_array!["GET", cookie.value()]))
+                        .map_err(Error::from)
+                        .then(move |res| {
+                            match res {
+                                Ok(val) => {
+                                    println!("{:?}", val);
+                                    true
+                                },
+                                Err(err) => {
+                                    false
+                                }
+                            }
+                        })
+                }).wait();
+
+                return true;
+            }
+        }
+    }
+
+    false
 }
